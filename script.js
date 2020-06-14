f04debccb0681e94c680c81f1955f49653cc5213
keshari777 <30973236+keshari777@users.noreply.github.com>
2020-06-14 21:36:40 +0530

    js function added for taking photos on clicking the button

--- script.js
+++ script.js
@@ -7,7 +7,8 @@ const snap = document.querySelector('.snap');
 function getVideo(){
     navigator.mediaDevices.getUserMedia({video:true,audio:false})
         .then(localMediaStream=>{
-            video.src = window.URL.createObjectURL(localMediaStream);
+           // video.src = window.URL.createObjectURL(localMediaStream);
+           video.srcObject = localMediaStream;
             video.play();
         })
         .catch(err=>{
@@ -23,7 +24,26 @@ function paintToCanvas(){
 
     return setInterval(()=>{
         ctx.drawImage(video,0,0,width,height);
+        let pixels = ctx.getImageData(0,0,width,height);
+        //pixels=redEffect(pixels);
+       // debugger;
+      // pixels=rgbsplit(pixels);
+      // ctx.globalAlpha=0.8;
+      pixels=greenSplit(pixels);
+       ctx.putImageData(pixels,0,0);
     },16);
 }
+function takePhoto(){
+    snap.currentTime=0;
+    snap.play();
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href=data;
+    link.setAttribute('download','handsome');
+    link.innerHTML=`<img src="${data}" alt="Beautiful Women"/>`;
+    strip.insertBefore(link,strip.firstChild);
+}
+
 
 getVideo();
+video.addEventListener('canplay',paintToCanvas);
